c4a40445abbf75f18fa9f944400dae35d953f3c0
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-08-22 23:44:34 +0330

    set header responsive #f

---
+++ src/script/script.js
@@ -0,0 +1,6 @@
+// const burgurIcon = document.querySelector(".burgur-icon");
+// const smallMenuHover = document.getElementsByClassName("sml");
+
+// burgurIcon.addEventListener("click", () =>{
+//   smallMenuHover.classList.add("d-mo")
+// });
