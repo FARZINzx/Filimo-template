3fc87a58fa3418aa6f5410915102a2e28d09ce4c
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-08-26 14:54:40 +0330

    add script code for scroll to top btn

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,20 @@
-// const burgurIcon = document.querySelector(".burgur-icon");
-// const smallMenuHover = document.getElementsByClassName("sml");
+const burgurIcon = document.querySelector(".burger");
+const smallMenuHover = document.querySelector(".sml");
+let scrollTop = document.querySelector(".scroll-top")
 
-// burgurIcon.addEventListener("click", () =>{
-//   smallMenuHover.classList.add("d-mo")
-// });
+burgurIcon.addEventListener('mouseEnter',()=>{
+     smallMenuHover
+})
+
+const scrollWindow = function () {  
+     if (window.scrollY != 0) {
+       setTimeout(function () {
+         window.scrollTo(0, window.scrollY - 50);
+         scrollWindow();
+       }, 10);
+     }
+   };
+
+   scrollTop.addEventListener('click',scrollWindow)
+
+ 
